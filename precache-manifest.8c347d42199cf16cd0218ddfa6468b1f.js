466589ad834530024263148086ad33bee07ce2e6
Siddhant Jain <siddhantjain15298@gmail.com>
2020-04-26 20:08:57 +0530

    Updates

---
+++ precache-manifest.8c347d42199cf16cd0218ddfa6468b1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ace3c1eaf7fcfcad571e114bd74dad",
+    "url": "/face_RecognitionBrain/index.html"
+  },
+  {
+    "revision": "5da3022b194c7a4333d4",
+    "url": "/face_RecognitionBrain/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6dc4b63a8e7ad40a6161",
+    "url": "/face_RecognitionBrain/static/js/2.c09fca6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face_RecognitionBrain/static/js/2.c09fca6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da3022b194c7a4333d4",
+    "url": "/face_RecognitionBrain/static/js/main.3ce22d4b.chunk.js"
+  },
+  {
+    "revision": "d34690d7defd960624df",
+    "url": "/face_RecognitionBrain/static/js/runtime-main.3d32d74c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/face_RecognitionBrain/static/media/logo.5d5d9eef.svg"
+  }
+]);
