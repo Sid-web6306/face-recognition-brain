5d108c8b8fec8880de121de945546bab285d0d71
Siddhant Jain <siddhantjain15298@gmail.com>
2020-04-27 08:53:10 +0530

    adding Navigation"

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const navigation = () =>{
+    return(
+        <div style={{display:'flex',justifyContent:'flex-end'}}>
+            <p >Sign Out</p>
+        </div>
+    )
+
+}
+
+export default navigation;
